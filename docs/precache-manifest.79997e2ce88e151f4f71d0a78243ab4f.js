6975235522dbbaa8d0725714a1c8efbcd2380868
sa2taka <sa2taka@gmail.com>
2020-10-25 15:47:08 +0900

    add docs

---
+++ docs/precache-manifest.79997e2ce88e151f4f71d0a78243ab4f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b918d650f644ca5c5794",
+    "url": "css/app.bf9718a5.css"
+  },
+  {
+    "revision": "6ba32cefde62c4d30026",
+    "url": "css/chunk-vendors.ca802091.css"
+  },
+  {
+    "revision": "3d1f8fa2f06249540889a7bbe69cf5bb",
+    "url": "fonts/materialdesignicons-webfont.3d1f8fa2.eot"
+  },
+  {
+    "revision": "3e722fd57a6db80ee119f0e2c230ccff",
+    "url": "fonts/materialdesignicons-webfont.3e722fd5.ttf"
+  },
+  {
+    "revision": "4187121a4353440c2a865dbf1bc1901b",
+    "url": "fonts/materialdesignicons-webfont.4187121a.woff2"
+  },
+  {
+    "revision": "fec1b66adcf131415a2511bd77e747cc",
+    "url": "fonts/materialdesignicons-webfont.fec1b66a.woff"
+  },
+  {
+    "revision": "b0a8f011b9d141528ad75279150aac5f",
+    "url": "index.html"
+  },
+  {
+    "revision": "b918d650f644ca5c5794",
+    "url": "js/app.baf2f4d3.js"
+  },
+  {
+    "revision": "6ba32cefde62c4d30026",
+    "url": "js/chunk-vendors.3af3c665.js"
+  },
+  {
+    "revision": "3b394de7fb8a19142284ef53380e92f2",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
